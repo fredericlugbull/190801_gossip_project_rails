761a6eec66ba2c3360d2721a2660d5a3063a04a3
Frédéric <f.lugbull@gmail.com>
2019-08-02 01:06:33 +0200

    maj

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+10.times do |index|
+  User.create!(first_name: Faker::DcComics.hero, last_name: Faker::Movies::StarWars.character, description: "beau-gosse", email: Faker::Internet.email,  age: rand(20..60))
+end
+
+puts "J'ai créé 10 profils en automatisé avec la gem Faker, mate un peu le résulat!"
+puts tp User.all
